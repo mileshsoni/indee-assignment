07f65d982b1a659f2667dc36bc8b381993f0dbbb
mileshsoni <95475731+mileshsoni@users.noreply.github.com>
2024-05-12 22:13:42 +0530

    Update urls.py

--- songs/music/urls.py
+++ songs/music/urls.py
@@ -6,12 +6,12 @@ from .views import list_playlists
 from .views import edit_playlist, delete_playlist, list_playlist_songs, move_playlist_song, remove_playlist_song
 urlpatterns = [
     path('api/songs/', create_song),
-    path('api/available-songs/', list_songs),
-    path('api/create-playlist/', create_playlist),
-    path('api/list-playlist/', list_playlists),
-    path('api/rename-playlist/<int:playlist_id>/', edit_playlist, name = "edit-playlist"),
-    path('api/delete-playlist/<int:playlist_id>/', delete_playlist, name = "delete-playlist"),
-    path('api/list-playlist-songs/<int:playlist_id>', list_playlist_songs, name = "list-playlist-songs"),
-    path('api/move-playlist-songs/<int:playlist_id>/songs/<int:song_id>', move_playlist_song),
-    path('api/remove-playlist/<int:playlist_id>/song/<int:song_id>', remove_playlist_song),
+    path('api/songs/', list_songs),
+    path('api/playlists/', create_playlist),
+    path('api/playlists/', list_playlists),
+    path('api/playlists/<int:playlist_id>/', edit_playlist, name = "edit-playlist"),
+    path('api/playlists/<int:playlist_id>/', delete_playlist, name = "delete-playlist"),
+    path('api/playlists/<int:playlist_id>/songs', list_playlist_songs, name = "list-playlist-songs"),
+    path('api/playlists/<int:playlist_id>/songs/<int:song_id>', move_playlist_song),
+    path('api/playlists/<int:playlist_id>/songs/<int:song_id>', remove_playlist_song),
 ]
